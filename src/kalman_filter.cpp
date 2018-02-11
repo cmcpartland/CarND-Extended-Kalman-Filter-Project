a53bd186cdf49c97cf942c6d55f627f93892aa3f
Connor McPartland <cmcpartland91@gmail.com>
2018-02-11 14:48:47 -0500

    Update 1
    Remove unecessary calculation of P*Ht

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -78,9 +78,9 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
 
 void KalmanFilter::Estimate(const VectorXd &y) {
   MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
   MatrixXd PHt = P_ * Ht;
+  MatrixXd S = H_ * PHt + R_;
+  MatrixXd Si = S.inverse();
   MatrixXd K = PHt * Si;
 
   // new estimate
