5e7a547881dc44b098ea170562c6d2fced6d921e
Connor McPartland <cmcpartland91@gmail.com>
2021-08-01 10:47:15 +0200

    Update to the comments

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -14,33 +14,35 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+  x_ = x_in; // Current state
+  P_ = P_in; // Covariance of the state estimation
+  F_ = F_in; // State transition matrix - describes the dynamical equations governing the system
+  H_ = H_in; // Observation matrix - converts system state into outputs via matrix transformation
+  R_ = R_in; // Measurement uncertainty - covariance matrix of the measurement
+  Q_ = Q_in; // Process noise uncertainty
 }
 
 void KalmanFilter::Predict() {
   /**
   Predict the state
   */
-  // State extrapolation equation
-  x_ = F_ * x_;
+	
+  // State extrapolation equation - F_ is applied to the current value of x_ to give the next predicted state of x_
+  x_ = F_ * x_; 
 	
   MatrixXd Ft = F_.transpose();
 	
-  // Covariance extrapolation equation
+  // Covariance extrapolation equation - describes how the covariance of the estimation extrapolates to the next cycle
   P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
-  Update the state
+  Update the state based on a new measurement
   */
-  VectorXd z_pred = H_ * x_;
-  VectorXd y = z - z_pred; 
+	
+  VectorXd z_pred = H_ * x_; // Apply the observation matrix to the current state
+  VectorXd y = z - z_pred; // Difference between the measurement (z) and the predicted measurement we'd expect given the current state
   Estimate(y);
 }
 
@@ -51,6 +53,7 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   float vx = x_(2);
   float vy = x_(3);
   
+  // cylindrical coordinates
   float rho = sqrt(px*px + py*py);
   float theta = atan2(py,px);
   float rho_dot = (px*vx + py*vy)/rho;
@@ -83,8 +86,8 @@ void KalmanFilter::Estimate(const VectorXd &y) {
   MatrixXd K = PHt * Si;
 
   // new estimate
-  x_ = x_ + (K * y);
+  x_ = x_ + (K * y); // update to the state based on filtering equation
   int x_size = x_.size();
   MatrixXd I = MatrixXd::Identity(x_size, x_size);
-  P_ = (I - K * H_) * P_;
+  P_ = (I - K * H_) * P_; // Update to the covariance matrix 
 }
